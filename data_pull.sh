68dd27b7f395d414cc7ce8f41a3bb4425f01a462
lang0909 <lang0909@ajou.ac.kr>
2020-01-21 19:37:44 +0900

    modify shell script

--- data_pull.sh
+++ data_pull.sh
@@ -2,8 +2,7 @@ REPOSITORY="/home/ec2-user"
 cd $REPOSITORY
 kill -9 `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
 cd data
-cd FIFAONLINE_DATA
-git pull
+git pull origin master
 mongoimport --db fifaonline-top10000 --collection topRankerUsingAverage --drop --file topRankerUsingAverage.json
 cd $REPOSITORY/server/backend
 nohup npm start &
--- image_pull.sh
+++ image_pull.sh
@@ -2,7 +2,7 @@ REPOSITORY="/home/ec2-user"
 cd $REPOSITORY
 kill -9 `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
 cd image
-git pull
+git pull origin master
 cd ../
 mv image/players/ server/backend/players/
 mv image/playersAction/ server/backend/playersAction
