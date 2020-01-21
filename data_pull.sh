09b7bb658d018654f8e1752a6a0d67e6c69b619e
lang0909 <lang0909@ajou.ac.kr>
2020-01-21 16:29:34 +0900

    fix typo

--- data_pull.sh
+++ data_pull.sh
@@ -5,6 +5,6 @@ kill -9 `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
 cd data
 cd FIFAONLINE_DATA
 git pull origin master
-mongoimport --db fifaonline-top10000 --collection topRankerUsingAverage --drop --file topRankerUsingAverage.json --jsonArray
+mongoimport --db fifaonline-top10000 --collection topRankerUsingAverage --drop --file topRankerUsingAverage.json
 cd $REPOSITORY/server/backend
 nohup npm start &
